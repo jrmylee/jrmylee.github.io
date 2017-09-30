e2ecef4c669c9b9cb6c9966965e2e6283ce8c83a
Jeremy Lee <jrmylee@gmail.com>
2017-09-29 18:23:54 -0700

    Initial

---
+++ app.js
@@ -0,0 +1,46 @@
+var modelController = (function(){
+
+
+
+})();
+
+var uiController = (function(){
+
+  var DOMstrings = {
+    aboutme: '.about__me',
+  };
+
+  return{
+    getDOMstrings: function(){
+      return DOMstrings;
+    }
+  }
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl){
+
+  var setupEventListeners = function(){
+    var DOMstrings = UICtrl.getDOMstrings();
+    document.querySelector('.about__me').addEventListener('click', function(){
+      var modal = document.querySelector('.modal');  // assuming you have only 1
+      modal.classList.add('is-active');
+
+      modal.querySelector('.modal-background').addEventListener('click', function(e) {
+        e.preventDefault();
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+      });
+    });
+  };
+
+  return{
+    init: function() {
+      console.log("App has started");
+      setupEventListeners();
+    }
+  };
+
+})(modelController, uiController);
+
+controller.init();
