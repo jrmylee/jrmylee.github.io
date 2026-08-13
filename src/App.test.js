d13331d8e1ce8f933e7715766e5cfac5748d00a1
jrmylee <jrmylee@berkeley.edu>
2026-08-13 13:54:56 -0700

    flatten portfolio cards and add footer piano

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,7 @@ import App from './App';
 test('renders Jeremy Lee portfolio', () => {
   render(<App />);
   expect(screen.getByRole('heading', { name: /jeremy lee/i })).toBeInTheDocument();
-  expect(screen.getByText(/more info than you need to know/i)).toBeInTheDocument();
+  expect(screen.getByText(/i live in san francisco/i)).toBeInTheDocument();
+  expect(screen.queryByText(/more info than you need to know/i)).not.toBeInTheDocument();
+  expect(screen.queryByText(/online\/offline/i)).not.toBeInTheDocument();
 });
